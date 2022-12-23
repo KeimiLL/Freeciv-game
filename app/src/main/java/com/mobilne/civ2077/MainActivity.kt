dbe4fc2591d311977360e534394eaac6a34f9ea8
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-12-23 12:16:45 +0100

    IO-20: Add Jetpack Compose
     - Add dependencies

--- app/src/main/java/com/mobilne/civ2077/MainActivity.kt
+++ app/src/main/java/com/mobilne/civ2077/MainActivity.kt
@@ -2,10 +2,14 @@ package com.mobilne.civ2077
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text("echo działa")
+        }
     }
 }
