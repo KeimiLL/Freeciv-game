a54e1544147093958a6acfe6d910e8230c4a3b39
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-12-25 23:06:45 +0100

    IO-23: Hide bars and make landscape the default orientation

--- app/src/main/java/com/mobilne/civ2077/MainActivity.kt
+++ app/src/main/java/com/mobilne/civ2077/MainActivity.kt
@@ -1,15 +1,43 @@
 package com.mobilne.civ2077
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Text
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             Text("echo działa")
+        }
+
+        setFullScreen()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        setFullScreen()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        setFullScreen()
+    }
+
+    private fun setFullScreen() {
+        window.setDecorFitsSystemWindows(false)
+        val controller = window.insetsController
+        if (controller != null) {
+            controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            controller.systemBarsBehavior =
+                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
 }
