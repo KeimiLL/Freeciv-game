64c8374ca976d8b83234a94aafac540463604c93
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-01-02 21:33:45 +0100

    IO-26: Add basic project architecture with dependency injection

---
+++ app/src/main/java/com/mobilne/civ2077/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.mobilne.civ2077.di
+
+import android.content.Context
+import com.mobilne.civ2077.ui.BaseApplication
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app: Context): BaseApplication {
+        return app as BaseApplication
+    }
+}
---
+++ app/src/main/java/com/mobilne/civ2077/ui/BaseApplication.kt
@@ -0,0 +1,7 @@
+package com.mobilne.civ2077.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application()
