15ff403edf9706e1973e66eec17b4e8e61279729
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-01-09 20:41:22 +0100

    IO-35: Rename SendArmyDialog,create NationChoiceViewModel, hardcoded perks for nation choice in created vm

---
+++ app/src/main/java/com/mobilne/civ2077/ui/nation_choice/NationChoiceViewModel.kt
@@ -0,0 +1,8 @@
+package com.mobilne.civ2077.ui.nation_choice
+
+class NationChoiceViewModel {
+    val francePerk: String = "Your army can travel double distance each turn"
+    val spainPerk: String = "Every turn gain additional 100 gold"
+    val ukPerk: String = "Every Perk you buy cost 10% less gold"
+    val usaPerk: String = "Your army deal 10% more damage"
+}
